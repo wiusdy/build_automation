6cc31d7479c95fa44916d82a146eb46c32049dc9
William Sdayle Marins Silva <WS_MarinsSilva@freewheel.tv>
2025-11-25 18:25:24 -0300

    solving pre-commit issues

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__, template_folder="../web")
 
+
 @app.route("/")
 def home():
     message = "Hello CI/CD Pipeline! This is a mock AWS deployment."
